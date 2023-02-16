117e4b94b506a1133f40283bf1171d38c1b088ed
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-02-17 01:48:08 +0900

    practice registration bean

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,6 +4,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +14,21 @@ public class UserController {
 
     private final UserService userService;
 
+//    스프링 빈을 주입 받는 방법
+//    1. 생성자 사용 (스프링 버전이 업데이트 되면서 @Autowired 생략 가능)
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+//    2. setter 사용 (누군가 setter를 사용하면 오작동할 수 있다.)
+//    @Autowired
+//    public void setUserService(UserService userService) {
+//        this.userService = userService;
+//    }
+
+//    3. 필드에 바로 @Autowired 사용: 테스트를 어렵게 만드는 요인
+//    @Autowired
+//    private final UserService userService;
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
