ae85586631f4feb7dc0174f44a2ec51cc035218c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-02-19 20:12:54 +0900

    practice jpa and controller test

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,7 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.service.user.UserServiceV1;
+import com.group.libraryapp.service.user.UserServiceV2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,12 +11,12 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private final UserServiceV1 userServiceV1;
+    private final UserServiceV2 userServiceV2;
 
 //    스프링 빈을 주입 받는 방법
 //    1. 생성자 사용 (스프링 버전이 업데이트 되면서 @Autowired 생략 가능)
-    public UserController(UserServiceV1 userServiceV1) {
-        this.userServiceV1 = userServiceV1;
+    public UserController(UserServiceV2 userServiceV2) {
+        this.userServiceV2 = userServiceV2;
     }
 
 //    2. setter 사용 (누군가 setter를 사용하면 오작동할 수 있다.)
@@ -31,22 +31,22 @@ public class UserController {
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        userServiceV1.saveUser(request);
+        userServiceV2.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        return userServiceV1.getUsers();
+        return userServiceV2.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        userServiceV1.updateUser(request);
+        userServiceV2.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        userServiceV1.deleteUser(name);
+        userServiceV2.deleteUser(name);
     }
 
 }
